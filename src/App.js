d38539c755814a7f684a661eaa6ba9c809ea727b
kshitiz <kshitizchauhan38@gmail.com>
2022-01-07 21:13:30 +0530

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LoadingBar from "react-top-loading-bar";
+import { useState } from "react";
+import Home from "./components/Home";
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        Hello This is test code-2
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+              <Home/>
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
